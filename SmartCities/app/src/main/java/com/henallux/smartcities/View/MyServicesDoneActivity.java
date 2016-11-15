6ed313eea0fb9c0aac02cdebe89180581b9ffcb1
olivierBeguin <oli0795@hotmail.com>
2016-11-15 19:46:14 +0100

    Modif 15/11

--- SmartCities/app/src/main/java/com/henallux/smartcities/View/MyServicesDoneActivity.java
+++ SmartCities/app/src/main/java/com/henallux/smartcities/View/MyServicesDoneActivity.java
@@ -1,16 +1,19 @@
 package com.henallux.smartcities.view;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.henallux.smartcities.R;
 
-public class MyServicesDoneActivity extends AppCompatActivity {
-
+public class MyServicesDoneActivity extends Fragment
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_services_done);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_my_services_done, container, false);
     }
 }
+
